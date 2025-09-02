7e09d06d443cecdb1e7e3fa0ae8a80a869093477
csb6 <cblakley15@gmail.com>
2025-09-01 20:09:55 -0700

    Made date column of AccountModel into a QDate so it is displayed in the user's locale

--- models/AccountModel.cpp
+++ models/AccountModel.cpp
@@ -1,4 +1,5 @@
 #include "AccountModel.hpp"
+#include <QDate>
 #include "SQLColumns.hpp"
 
 AccountModel::AccountModel(QSqlDatabase& db, int account_id)
@@ -12,4 +13,17 @@ AccountModel::AccountModel(QSqlDatabase& db, int account_id)
     setFilter(QString("source = %1 or destination = %1").arg(account_id));
 
     select();
+}
+
+QVariant AccountModel::data(const QModelIndex& index, int role) const
+{
+    if(role != Qt::DisplayRole) {
+        return QSqlRelationalTableModel::data(index, role);
+    }
+
+    if(index.column() == TRANSACTIONS_DATE) {
+        auto isoDateStr = QSqlRelationalTableModel::data(index, role).toString();
+        return QDate::fromString(isoDateStr, Qt::ISODate);
+    }
+    return QSqlQueryModel::data(index, role);
 }
--- models/AccountModel.hpp
+++ models/AccountModel.hpp
@@ -7,4 +7,6 @@ class AccountModel : public QSqlRelationalTableModel {
     Q_OBJECT
 public:
     AccountModel(QSqlDatabase&, int account_id);
+
+    QVariant data(const QModelIndex&, int role) const override;
 };
