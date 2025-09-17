6b9228cb028d2ac349c12c189dfea6260ec42e0b
csb6 <cblakley15@gmail.com>
2025-09-16 19:57:56 -0700

    Made AccountTree implementation somewhat more comprehensible
    Still not really satisfied with it but it at least seems to be flatter

--- src/models/AccountTree.cpp
+++ src/models/AccountTree.cpp
@@ -18,7 +18,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "AccountTree.hpp"
 #include <algorithm>
-#include <iterator>
 #include <vector>
 #include <QSqlQueryModel>
 #include <QSqlDatabase>
@@ -77,24 +76,23 @@ void AccountTree::load()
     build_tree(m_impl->query_model, invisibleRootItem());
 }
 
+// Assumes query orders the accounts by name (ascending)
 static
 void build_tree(QSqlQueryModel& query_model, QStandardItem* root)
 {
-    std::vector<QString> account_name_stack;
+    std::vector<QString> account_name_stack{u""_s};
     std::vector<QStandardItem*> account_stack{root};
     for(int row = 0; row < query_model.rowCount(); ++row) {
         auto account_id = query_model.index(row, ACCOUNTS_ID).data().toInt();
         auto account_path = query_model.index(row, ACCOUNTS_NAME).data().toString();
         auto parts = account_path.split('/');
-        // Pop items from the stack until the stack is a prefix of parts
-        // This means we are at a point in the tree where we can add new parts
-        auto[mismatch, first_new_part] = std::ranges::mismatch(account_name_stack, parts);
-        size_t delete_count = std::distance(mismatch, account_name_stack.end());
-        for(size_t i = 0; i < delete_count; ++i) {
-            account_stack.pop_back();
-            account_name_stack.pop_back();
-        }
-        // Add all of the new parts to the tree (and the stack)
+        // Find the point where the stack and account_path differ.
+        // This is the point in the path where new parts need to be added to the tree.
+        auto[first_to_delete, first_new_part] = std::mismatch(account_name_stack.begin() + 1, account_name_stack.end(), parts.begin(), parts.end());
+        size_t start_pt = first_to_delete - account_name_stack.begin();
+        account_stack.erase(account_stack.begin() + start_pt, account_stack.end());
+        account_name_stack.erase(account_name_stack.begin() + start_pt, account_name_stack.end());
+        // Add the new parts to the tree
         for(auto part = first_new_part; part != parts.end(); ++part) {
             auto* new_item = new QStandardItem(*part);
             account_stack.back()->appendRow(new_item);
